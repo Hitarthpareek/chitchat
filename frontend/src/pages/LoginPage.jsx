90337540b685173fcfec8a44a142ff5bc645751a
eve_coder <hitarthpareek10@gmail.com>
2026-03-13 02:08:54 +0530

    frontend lib installed and ready for dev

---
+++ frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { useAuthStore } from '../store/useAuthStore';
+
+export function LoginPage() {
+  
+  return (
+    <div>LoginPage</div>
+  )
+}
+
+export default LoginPage
